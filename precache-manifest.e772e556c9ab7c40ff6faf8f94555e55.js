3c05db9a10f55b76c211acfb756acfea39e11eb8
mwhocodes <mwhocodes@users.noreply.github.com>
2021-02-20 21:30:56 +0700

    Updates

---
+++ precache-manifest.e772e556c9ab7c40ff6faf8f94555e55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ea8c6d65df20b68f7c734d1d8d58a78",
+    "url": "/t0-do-list/index.html"
+  },
+  {
+    "revision": "b68977a061f00897ecd4",
+    "url": "/t0-do-list/static/css/main.42f9860d.chunk.css"
+  },
+  {
+    "revision": "0102358de18014d75f59",
+    "url": "/t0-do-list/static/js/2.005ae40d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/t0-do-list/static/js/2.005ae40d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b68977a061f00897ecd4",
+    "url": "/t0-do-list/static/js/main.458a66dd.chunk.js"
+  },
+  {
+    "revision": "d912c86383b26db9538c",
+    "url": "/t0-do-list/static/js/runtime-main.6350be9d.js"
+  }
+]);
